7fd0f1daadfd0f83836f2b83f605ec77a9e9d524
kolbasator <76650533+kolbasator@users.noreply.github.com>
2021-04-12 21:06:32 +0600

    joins

--- Joins/Task5.sql
+++ Joins/Task5.sql
@@ -1,3 +1,2 @@
-SELECT date_of_rental,date_of_return,last_name
-FROM rentals
-NATURAL JOIN clients
+SELECT last_name,title FROM movies m,clients c,rentals r,copies cp
+WHERE c. client_id = r. client_id AND cp. copy_id = r. copy_id AND m. movie_id = cp. movie_id
---
+++ Joins/Task7.sql
@@ -0,0 +1,2 @@
+SELECT title,year FROM movies m,clients c,rentals r,copies cp
+WHERE c. client_id = r. client_id AND cp. copy_id = r. copy_id AND m. movie_id = cp. movie_id AND c. first_name = 'Gary'
